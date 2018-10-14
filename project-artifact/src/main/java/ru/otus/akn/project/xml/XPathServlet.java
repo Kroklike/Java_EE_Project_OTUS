0fbd2f1507051913adc34617ca795d2dd6510e49
Kroklike <IAMKIRART@mail.ru>
2018-10-14 18:57:39 +0300

    Get employees with more than average salary

--- project-artifact/src/main/java/ru/otus/akn/project/xml/XPathServlet.java
+++ project-artifact/src/main/java/ru/otus/akn/project/xml/XPathServlet.java
@@ -1,6 +1,7 @@
 package ru.otus.akn.project.xml;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,24 +10,41 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.IOException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+import java.io.PrintWriter;
 
+import static ru.otus.akn.project.db.util.ResourceUtil.getResourceFile;
 import static ru.otus.akn.project.xml.MarshalXMLServlet.FILE_TO_SAVE_EMPLOYEES_OBJECT;
 
 @WebServlet("/xPathEmployeeSearching")
 public class XPathServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder;
-        Document doc = null;
+        Document doc;
 
         try {
             builder = factory.newDocumentBuilder();
-            doc = builder.parse(FILE_TO_SAVE_EMPLOYEES_OBJECT);
+            doc = builder.parse(getResourceFile(this, FILE_TO_SAVE_EMPLOYEES_OBJECT));
 
+            XPathFactory xPathFactory = XPathFactory.newInstance();
+            XPath xPath = xPathFactory.newXPath();
+            XPathExpression expression = xPath.compile("/employees/employee" +
+                    "[salary > (sum(/employees/employee/salary) div count(/employees/employee))]/@employeeId");
 
+            NodeList result = (NodeList) expression.evaluate(doc, XPathConstants.NODESET);
+
+            try (PrintWriter pw = resp.getWriter()) {
+                pw.println("Employees with more than average salary:");
+                for (int i = 0; i < result.getLength(); i++) {
+                    pw.println("-Employee number: " + result.item(i).getNodeValue());
+                }
+            }
 
         } catch (Exception e) {
             throw new ServletException("Got exception when tried use xpath.", e);
